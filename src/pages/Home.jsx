a23217bdcfee98306f58260ea039306010053e42
lcgavilan97 <luciano.cortes05@inacapmail.cl>
2025-06-23 02:15:35 -0400

    Segundo commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,9 @@ function Home() {
     <div className="container mt-4">
       <h1>Productos Disponibles</h1>
       <CardProducto nombre="Pan Integral" precio="$500" />
+      <h2>Bienvenido a EcoFood</h2>
+      <CerrarSesion />
+
     </div>
   );
 }
